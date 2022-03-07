f500b2341d5e4d29e4e556406ce2716351a84590
Thomas Laporte <1976489@etu.cegep-lanaudiere.qc.ca>
2022-03-07 16:25:48 -0500

    premier envoie

---
+++ Atelier3-Planete/Planete.cs
@@ -0,0 +1,149 @@
+﻿/**
+* But: Classe Planete, qui contient et retourne les caractéristiques physiques et des méthodes de comparaison.
+* Auteur: Thomas Laporte
+* Date: 23/02/2022 
+**/
+
+using System;
+using System.Collections.Generic;
+
+namespace Atelier3_Planete
+{
+    class Planete
+    {
+        const double MASSE_TERRESTRE = 5.9722 * 10e+24; // Constante de la masse terrestre, unité de mesure.
+
+        private String m_nom; // Nom de la planète.
+        private int m_rayon; // Rayon de la planète, en km.
+        private double m_masse; // Masse de la planète, en masses terrestres.
+        private List<string> satellites;
+
+        // --Constructeurs--
+
+        /**
+         * Constructeur avec le nom seulement.
+         * Paramètre est le nom en String.
+         **/
+        public Planete(String nom)
+        {
+            m_nom = nom.Trim();
+        }
+
+        /**
+         * Constructeur avec le nom et la rayon.
+         * Paramètres sont le nom en String et le rayon en int.
+         * On vérifie que le rayon est positif.
+         **/
+        public Planete(String nom, int rayon)
+        {
+            m_nom = nom.Trim();
+
+            if (rayon > 0)
+                m_rayon = rayon;
+        }
+
+        /**
+         * Constructeur avec le nom et la rayon.
+         * Paramètres sont le nom en String, le rayon en int et la masse en double.
+         * On vérifie que le rayon et la masse sont positifs.
+         **/
+        public Planete(String nom, int rayon, double masse)
+        {
+            m_nom = nom.Trim();
+
+            if (rayon > 0)
+                m_rayon = rayon;
+
+            if (masse > 0)
+                m_masse = masse;
+        }
+
+        // --Accesseurs--
+
+        /**
+         * Accesseur du nom en lecture-écriture.
+         * Retourne le nom.
+         * Modifie le nom si le nombre de caractère est positif.
+         **/
+        public String Nom
+        {
+            get { return m_nom; }
+            set { if (value.Length > 0) { m_nom = value.Trim(); } }
+        }
+
+        /**
+         * Accesseur du rayon en lecture-écriture.
+         * Retourne le rayon (en km).
+         * Modifie le rayon s'il est positif.
+         **/
+        public int Rayon
+        {
+            get { return m_rayon; }
+            set { if (value > 0) { m_rayon = value; } }
+        }
+
+        /**
+         * Accesseur de la masse en lecture-écriture.
+         * Retourne la masse (en kg).
+         * Modifie la masse si elle est positive.
+         **/
+        public double Masse
+        {
+            get { return m_masse * MASSE_TERRESTRE; }
+            set { if (value > 0) { m_masse = value; } }
+        }
+
+        /**
+         * Accesseur de la superficie en lecture.
+         * Retourne la superficie (en km).
+         **/
+        public int Superficie
+        {
+            get { return (int)(4 * Math.PI * Math.Pow(m_rayon, 2)); }
+        }
+
+        /**
+         * Accesseur du volume en lecture.
+         * Retourne le volume (en km).
+         **/
+        public long Volume
+        {
+            get { return (long)(4 * Math.PI * Math.Pow(m_rayon, 3) / 3); }
+        }
+
+        /**
+        * Accesseur de la masse volumique en lecture.
+        * Retourne la masse volumique (en kg/km^3).
+        **/
+        public double MasseVolumique
+        {
+            get { if (m_rayon > 0 && m_masse > 0) { return (Masse * MASSE_TERRESTRE / Volume); } else { return 0; } }
+        }
+
+        // --Méthodes--
+
+        /**
+        * Retourner la planète la plus volumineuse.
+        * Paramètre de la planète à comparer.
+        **/
+        public Planete PlusVolumineuse(Planete planete)
+        {
+            if (this.Volume > planete.Volume)
+                return this;
+
+            return planete;
+        }
+
+        /**
+         * Retourner la planète la plus dense (celle avec la plus grande masse volumique).
+         * Paramètre de la planète à comparer.
+         **/
+        public Planete PlusDense(Planete planete)
+        {
+            if (this.MasseVolumique > planete.MasseVolumique)
+                return this;
+
+            return planete;
+        }
+    }
+}
---
+++ Atelier3-Planete/Programme.cs
@@ -0,0 +1,37 @@
+﻿/**
+* But: Classe Planete, qui contient et retourne les caractéristiques physiques et des méthodes de comparaison.
+* Auteur: Thomas Laporte
+* Date: 23/02/2022 
+**/
+
+using System;
+
+namespace Atelier3_Planete
+{
+    
+    class Programme
+    {
+        static void Main(string[] args)
+        {
+            Planete p1 = new Planete("Hellooo", 6371);
+            Planete p2 = new Planete("chicago", 994, 9.5);
+            Planete p3 = new Planete("bbbb", 6371);
+            Planete p4 = new Planete("aaaa", 99, 44444.5);
+            Planete p5 = new Planete("Hellooo", 6371);
+            Planete p6 = new Planete("azzz", 99, 44444.5);
+
+            p1.Masse = 4.5;
+
+            Console.WriteLine(p1.Nom);
+            Console.WriteLine(p1.Rayon);
+            Console.WriteLine(p1.Masse);
+            Console.WriteLine(p1.Superficie);
+            Console.WriteLine(p1.Volume);
+            Console.WriteLine(p1.MasseVolumique);
+            Console.WriteLine(p1.PlusVolumineuse(p2).Nom);
+            Console.WriteLine(p2.PlusDense(p4).Nom);
+
+            Console.ReadLine();
+        }
+    }
+}
